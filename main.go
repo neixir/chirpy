2ad46d8cecdb618e2b52a60fdfd35f87b681ba13
neixir <neixir@gmail.com>
2025-06-24 11:06:48 +0200

    CH3 L4 Admin Namespace

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 // CH1 L08 https://www.boot.dev/lessons/20709716-4d7c-47fe-b182-9bccf8436ddc
 // CH1 L11 https://www.boot.dev/lessons/174d13f0-f887-46c6-a633-d963662fde39
 // CH2 L01 https://www.boot.dev/lessons/a13ffa72-18b9-49f7-81a9-c5a17d007b3a
+// CH3 L04 https://www.boot.dev/lessons/892b38f7-d154-4591-ac63-a9fbc2a38187
 
 // CH2 L01
 type apiConfig struct {
@@ -28,13 +29,10 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 // CH1 L11
 func handlerHealth(w http.ResponseWriter, r *http.Request) {
-	// Write the Content-Type header
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
-	// Write the status code using w.WriteHeader
 	w.WriteHeader(200)
 
-	// Write the body text using w.Write
 	//fmt.Fprintf(w, "OK")
 	w.Write([]byte("OK"))
 
@@ -45,8 +43,23 @@ func fileserverHandle() http.Handler {
 	return http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 }
 
+// ... + CH3 L04
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	//fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	html :=
+		`
+<html>
+	<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+	</body>
+</html>
+`
+	fmt.Fprintf(w, html, cfg.fileserverHits.Load())
+
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
@@ -62,8 +75,8 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileserverHandle()))
 	mux.Handle("/assets", http.FileServer(http.Dir("./assets"))) // CH1 L05
 	mux.HandleFunc("GET /api/healthz", handlerHealth)
-	mux.HandleFunc("GET /api/metrics", apiCfg.metricsHandler) // CH2 L1
-	mux.HandleFunc("POST /api/reset", apiCfg.resetHandler)    // CH2 L1
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler) // CH2 L1
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)    // CH2 L1
 
 	// Create a new http.Server struct.
 	server := &http.Server{
