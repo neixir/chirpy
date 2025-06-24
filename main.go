e28ff7d33eb50412b94b24691524dc115b4af21f
neixir <neixir@gmail.com>
2025-06-24 08:29:58 +0200

    CH1 L08 Serving images

--- main.go
+++ main.go
@@ -1,35 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
-// CH1 L4 https://www.boot.dev/lessons/861ada77-c583-42c8-a265-657f2c453103
-// CH1 L5 https://www.boot.dev/lessons/8cf7315a-ffc0-4ce0-b482-5972ab695131
+// CH1 L04 https://www.boot.dev/lessons/861ada77-c583-42c8-a265-657f2c453103
+// CH1 L05 https://www.boot.dev/lessons/8cf7315a-ffc0-4ce0-b482-5972ab695131
+// CH1 L08 https://www.boot.dev/lessons/20709716-4d7c-47fe-b182-9bccf8436ddc
 
 func main() {
-	// 1. Create a new http.ServeMux
-	// https://pkg.go.dev/net/http#NewServeMux
+	// CH1 L4-L5
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets"))) // CH1 L05
 
-	// CH1 L5
-	// http: attempting to traverse a non-directory
-
-	// 2. Create a new http.Server struct.
-	server := http.Server{
-		// 2.1 Use the new "ServeMux" as the server's handler
+	// Create a new http.Server struct.
+	server := &http.Server{
 		Handler: mux,
-		// 2.2 Set the .Addr field to ":8080"
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 
-	// 3. Use the server's ListenAndServe method to start the server
-	server.ListenAndServe()
+	// Use the server's ListenAndServe method to start the server
+	log.Fatal(server.ListenAndServe())
 
-	// 4. Build and run your server (e.g. go build -o out && ./out)
-
-	// 5. Open http://localhost:8080 in your browser.
-	// You should see a 404 error because we haven't connected any handler logic yet.
-	// Don't worry, that's what is expected for the tests to pass for now.
 }
